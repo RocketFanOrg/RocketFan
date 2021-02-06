c965316bd9f9aa4db0d498591b9fc2b1ecb4a8cf
RobertJClegg <rclegg@jhb.dvt.co.za>
2021-02-06 15:33:44 +0200

    Initial Commit

---
+++ RocketFan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RocketFan
+//
+//  Created by Robert Clegg on 2021/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RocketFan/RocketFanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketFanApp.swift
+//  RocketFan
+//
+//  Created by Robert Clegg on 2021/02/06.
+//
+
+import SwiftUI
+
+@main
+struct RocketFanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
