adba24619c5412aacdaf0b0c41b678e77466a55b
RJ-Clegg <Robert.clegg@gmail.com>
2021-07-05 17:02:26 +0200

    Updated main content view

--- RocketFan/ContentView.swift
+++ RocketFan/ContentView.swift
@@ -1,19 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    TabView {
-      UpcomingLaunchesView()
-        .tabItem {
-          Text("Upcoming")
-          Image(systemName: "calendar.badge.clock")
+    var body: some View {
+        TabView {
+            LaunchesView(filter: .future)
+                .tabItem {
+                    Text("Upcoming")
+                    Image(systemName: "calendar.badge.clock")
+                }
+
+            LaunchesView(filter: .past)
+                .tabItem {
+                    Text("Past")
+                    Image(systemName: "clock.arrow.circlepath")
+                }
         }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
